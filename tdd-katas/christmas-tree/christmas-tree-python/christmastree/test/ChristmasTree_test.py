0d3f3448ae5460bb0f495f4256300e9c7fe236c9
markspijkermannavara <mark.spijkerman@navara.nl>
2025-10-21 14:52:51 +0200

    initial setup

---
+++ tdd-katas/christmas-tree/christmas-tree-python/christmastree/test/ChristmasTree_test.py
@@ -0,0 +1,11 @@
+import pytest
+from hamcrest import *
+from ChristmasTree import ChristmasTree
+
+
+class TestChristmasTree:
+
+  def test_a_new_christmastree(self):
+      assert True is False
+      # Hamcrest style
+      # assert_that(True, equal_to(False))
